8d676f243d9cb52e03317a6aac1a53cda2dbfc4c
Vinicius Silva Alencar <vinicius.s.alencar@outlook.com>
2020-10-07 19:34:14 -0400

    feat: create a login, sign up and home view controller

---
+++ expanse/View Controllers/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  expanse
+//
+//  Created by Vinicius Alencar on 07/10/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+}
---
+++ expanse/View Controllers/SingUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SingUpViewController.swift
+//  expanse
+//
+//  Created by Vinicius Alencar on 07/10/20.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var secondNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var singUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func singUpTapped(_ sender: UIButton) {
+    }
+    
+}
